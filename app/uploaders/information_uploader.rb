05399274cac2faf57721610bd9a8a3f77f42efa9
Eric <twinpaireric@gmail.com>
2018-01-28 21:34:44 -0800

    Resize images to proper size as they are uploaded

--- app/uploaders/information_uploader.rb
+++ app/uploaders/information_uploader.rb
@@ -1,7 +1,7 @@
 class InformationUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -25,8 +25,9 @@ class InformationUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
+  # Image will be resized to width 1500 and it's proportion width
+  process resize_to_limit: [1500, 400000000]
+  
   # def scale(width, height)
   #   # do something
   # end
@@ -37,10 +38,9 @@ class InformationUploader < CarrierWave::Uploader::Base
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
